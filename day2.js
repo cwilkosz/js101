9c54efa2ee2b3c64744122ce536cf0fff3e226a9
C Wilkosz <theewilkyway@gmail.com>
2017-01-24 20:49:11 -0600

    finalpush

---
+++ day2.js
@@ -0,0 +1,87 @@
+//switch statement
+
+//var day ="Friday";
+
+//switch(day){
+//	case "Monday":
+//	console.log("Boooo");
+//	case "Friday":
+//	console.log("Party");
+//	break;
+//	default:
+
+//	console.log("not monday");
+//}
+
+//	if(day === "Monday"){
+//		console.log("Boooo");
+//	} else if(day === "Friday"){
+//		console.log("Party");
+//	} else{
+//		console.log("not monday");
+//	}
+
+var fruit = ["cherries", "apples", "bananas"];
+var fruitString = fruit.join(" cats");
+console.log("fruitString", fruitString);
+
+var catColors = 'green,red,yellow,black';
+var catColorsArray = catColors.split("'");
+console.log("catColorsArray", catColorsArray);
+
+var reverseFruit = fruit.reverse();
+console.log("reverseFruit", reverseFruit);
+
+var alphaFruit = fruit.sort();
+console.log("alphaFruit", alphaFruit);
+
+var nums =[9,6,8,2,11,66];
+var numSort = nums.sort(
+	function(a,b){
+		return a-b;
+	}
+	);
+
+console.log("numSort", numSort);
+
+var mixedCrap = ["r", "m", 33, 9];
+console.log("????",mixedCrap.sort());
+
+var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+console.log("bananas index", animals.indexOf("fish"));
+
+var animals2 = animals.slice(2,4);
+console.log("animals2", animals2);
+
+animals.splice(1, 1);
+console.log("no turtles", animals);
+
+
+var colors = ["red", "blue", "gold", "pink", "orange"];
+//sort, reverse make it a string
+var finalAnswer = colors.sort().reverse().join(" cat ");
+console.log ("finalAnswer", finalAnswer);
+//red cat pi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
